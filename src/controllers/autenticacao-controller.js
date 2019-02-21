7918e6096a5e5dbd1f1cd577e14888b24be4f950
ralphGSC <47863700+ralphGSC@users.noreply.github.com>
2019-02-21 13:59:50 -0300

    init

---
+++ src/controllers/autenticacao-controller.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var base64 = require('base-64');
+var utf8 = require('utf8');
+var ipconfig = require('ip');
+
+const repository = require('../repositories/autenticacao-repository');
+const authService = require('../services/auth-service');
+
+exports.post = async (req, res, next) => {
+    try {
+        let bytes = utf8.encode(req.body.SENHA);
+        let senhaEncode = base64.encode(bytes);
+        let agent = req.headers['user-agent'];
+        let ip = ipconfig.address();         
+      
+        // Verifica se foi informado o COD_SISTEMA no body
+        if(req.body.COD_SISTEMA == undefined){
+            res.status(400).send({              
+                MSN: 'Favor informar código do sistema'
+            });
+            return;
+        }  
+     
+         // Obtém dados do usuário
+        let usuario = await repository.getLogIN({
+            LOGIN: req.body.LOGIN,
+            SENHA: senhaEncode
+        });
+
+        // Verificar se autenticação
+        if (!usuario) {
+            res.status(404).send({              
+                MSN: 'Usuário e/ou senha inválido(s)'
+            });
+            return;
+        }
+
+        // Retornar as permissões do usuário
+        let permissoes = await repository.getPermissoes({
+            COD_USUARIO: usuario.COD_USUARIO,
+            COD_SISTEMA: req.body.COD_SISTEMA
+        });           
+
+        if (permissoes.length == 0) {
+            res.status(404).send({              
+                MSN: 'Acesso negado ao sistema: ' + req.body.COD_SISTEMA 
+            });
+            return;
+        }
+
+        // Inserir log de entrada no sistema
+        await repository.insSPD_LOG_LOGON({
+            COD_USUARIO: usuario.COD_USUARIO,
+            COD_SISTEMA: req.body.COD_SISTEMA,
+            AGENTE: agent,
+            IP: ip               
+        });            
+
+        const token = await authService.encodeToken({
+            COD_USUARIO: usuario.COD_USUARIO,
+            LOGIN: usuario.LOGIN,
+            MATRICULA: usuario.MATRICULA
+        });
+
+        res.status(201).send({
+            TOKEN: token,
+            USUARIO: {
+                COD_USUARIO: usuario.COD_USUARIO,
+                LOGIN: usuario.LOGIN,
+                MATRICULA: usuario.MATRICULA,
+                NOME_USUARIO: usuario.NOME_USUARIO,
+                EMAIL: usuario.EMAIL
+            },
+            PERMISSOES: permissoes
+        });
+       
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({           
+            MSN: 'Falha ao processar requisição'
+        });
+    }
+};
---
+++ src/repositories/autenticacao-repository.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const db = require('../services/database-service');
+
+exports.getLogIN = async (data) => {   
+    const res = await db.comandText(`SELECT COD_USUARIO, LOGIN, 
+                                            MATRICULA, NOME_USUARIO, EMAIL
+                                       FROM SPD_USUARIO 
+                                      WHERE LOGIN = '${data.LOGIN}' AND SENHA = '${data.SENHA}'`);
+    return res.rows[0];
+}
+
+exports.insSPD_LOG_LOGON = async (data) => {
+    const res = await db.comandText(`INSERT INTO SPD_LOG_LOGON 
+                                    (COD_USUARIO, COD_SISTEMA, AGENTE, IP)
+                                    VALUES
+                                    (${data.COD_USUARIO}, ${data.COD_SISTEMA}, '${data.AGENTE}', '${data.IP}')`);
+    return res;
+}
+
+exports.getPermissoes = async (data) => {
+    const res = await db.comandText(`SELECT DISTINCT MD.COD_MODULO, MD.DESC_MODULO, MD.COD_ORDEM, MD.DESC_EXIBICAO 
+                                       FROM SPD_PERFIL_ACESSO PA, SPD_PERFIL PF, SPD_MODULO_SUB MS, SPD_MODULO MD, SPD_SISTEMA ST 
+                                      WHERE PA.COD_PERFIL = PF.COD_PERFIL
+                                        AND PA.COD_SUBMODULO = MS.COD_SUBMODULO
+                                        AND MS.COD_MODULO = MD.COD_MODULO 
+                                        AND MD.COD_SISTEMA = ST.COD_SISTEMA 
+                                        AND PA.COD_PERFIL IN 
+                                        (SELECT COD_PERFIL FROM SPD_USUARIO_PERFIL WHERE COD_USUARIO = ${data.COD_USUARIO})  
+                                        AND MD.COD_SISTEMA = ${data.COD_SISTEMA} ORDER BY MD.COD_ORDEM`);
+    return res.rows;
+}
